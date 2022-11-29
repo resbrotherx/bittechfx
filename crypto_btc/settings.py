c02ef14003985b7f4ff96dc97f480e83a5bae3e6
resbrotherx <francisdaniel140@gmail.com>
2022-11-28 20:07:54 -1200

    updated

--- crypto_btc/settings.py
+++ crypto_btc/settings.py
@@ -164,21 +164,21 @@ MESSAGE_TAGS = {
 
 # 465
 # 587 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'mail.privateemail.com'
+EMAIL_PORT = 465
+EMAIL_HOST_USER = 'support@bittechfx.com'
+EMAIL_HOST_PASSWORD = 'Bittechfx.x101'
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
+
 # EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST = 'mail.privateemail.com'
+# EMAIL_HOST = 'smtp.titan.email'
 # EMAIL_PORT = 465
 # EMAIL_HOST_USER = 'support@bitechfx.com'
 # EMAIL_HOST_PASSWORD = 'Bitechfx.x101'
 # EMAIL_USE_TLS = False
 # EMAIL_USE_SSL = True
-
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.titan.email'
-EMAIL_PORT = 465
-EMAIL_HOST_USER = 'support@bitechfx.com'
-EMAIL_HOST_PASSWORD = 'Bitechfx.x101'
-EMAIL_USE_TLS = False
-EMAIL_USE_SSL = True
 
 # EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 # EMAIL_HOST = 'smtp.gmail.com'
