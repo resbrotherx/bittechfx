7dc033ccf5fd4c80405ea5f3eafb8e3e993bdcd3
Resbrotherx <francisdaniel140@gmail.com>
2025-03-14 16:24:39 +0100

    updated

--- crypto_btc/settings.py
+++ crypto_btc/settings.py
@@ -132,8 +132,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/staticfiles/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
