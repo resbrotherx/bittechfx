d293a141acc4001e169cd267c4822316fcb646af
Resbrotherx <francisdaniel140@gmail.com>
2025-03-16 17:21:28 +0100

    updated

--- crypto_btc/settings.py
+++ crypto_btc/settings.py
@@ -184,10 +184,10 @@ MESSAGE_TAGS = {
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-# EMAIL_HOST_USER = 'Bittechfxhub@gmail.com'
-# EMAIL_HOST_PASSWORD = 'joqg ejwl rach kbcf'
-EMAIL_HOST_USER = 'francisdaniel140@gmail.com'
-EMAIL_HOST_PASSWORD = 'ztzb xysj xexh zntd'
+EMAIL_HOST_USER = 'bittechfxhub@gmail.com'
+EMAIL_HOST_PASSWORD = 'joqg ejwl rach kbcf'
+# EMAIL_HOST_USER = 'francisdaniel140@gmail.com'
+# EMAIL_HOST_PASSWORD = 'ztzb xysj xexh zntd'
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 
